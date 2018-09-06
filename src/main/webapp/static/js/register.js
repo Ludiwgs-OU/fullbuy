bd845d0e79781f6538e95fbe0ff1263fa920a9f5
GRATTFISH <38063466+GRATTFISH@users.noreply.github.com>
2018-09-06 17:30:17 +0800

    改版注册

--- src/main/webapp/static/js/register.js
+++ src/main/webapp/static/js/register.js
@@ -1,27 +1,8 @@
-/*
- * 获取路径参数
- */
-function getUrlParam(name) {
-    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
-    var r = window.location.search.substr(1).match(reg);
-    if (r != null) return decodeURI(r[2]); return null; 
-}
-
 $(document).ready(()=>{
 	
-	$(function(){
-		var type = getUrlParam('type');
-		if(type == 1){
-			$("#member").show();
-		}
-		else if(type == 2){
-			$("#merchant").show();
-		}
-		else if(type == 3){
-			$("#pwd").show();
-		}
+	$(function() {
+		$('#doc-my-tabs').tabs();
 	})
-	
 	/*
 	 * 获取验证码
 	 */
@@ -32,86 +13,28 @@ $(document).ready(()=>{
 			alert("请输入正确的手机号码！");
 		}
 		else{
-			alert(phone);
+			$.ajax({
+				type:"post",
+				url:"",
+				async:true,
+				dataType:"json",
+				data: {
+					"phone": phone
+				},
+				success:function(result){
+					alert("已发送验证码，请注意查收！");
+				},
+				error:function(result){
+					alert("验证码发送失败！");
+				}
+			});
 		}
 	})
 	$("#merchant_btn").click(()=>{
-		var phone = $("#merchant_mphone").val();
-		var pattern = /^\d{11}$/;
-		if(!pattern.test(phone)){
-			alert("请输入正确的手机号码！");
-		}
-		else{
-			alert(phone);
-		}
-	})
-	$("#pwd_btn").click(()=>{
-		var phone = $("#pwd_phone").val();
-		var pattern = /^\d{11}$/;
-		if(!pattern.test(phone)){
-			alert("请输入正确的手机号码！");
-		}
-		else{
-			alert(phone);
-		}
-	})
-	
-	/*
-	 * 会员注册
-	 */
-	$("#member_register").click(()=>{
-		var phone = $("#member_phone").val();
-		var code = $("#member_code").val();
-		var memberReg = {
-			"loginName": phone,
-			"code": code
-		};
-		
-		$.ajax({
-			type:"post",
-			url:"",
-			async:true,
-			dataType:"json",
-			data: memberReg,
-			success:function(result){
-				alert("注册成功！");
-				history.go(-1);
-			},
-			error:function(result){
-				alert("注册失败！");
-			}
-		});
-	})
-	
-	/*
-	 * 商家注册
-	 */
-	$("#merchat_register").click(()=>{
-		var username = $("#merchant_mphone").val();
-		var name = $("#merchant_name").val();
-		var address = $("#merchant_address").val();
 		var phone = $("#merchant_phone").val();
-		var pass = $("#merchant_pass").val();
-		var repass = $("#repass").val();
-		var des = $("#merchant_des").val();
-		var code = $("#merchant_code").val();
-		
-		var merchantReg = {
-			"username": username,
-			"salesName": name,
-			"selesPhone": phone,
-			"salesAddress": address,
-			"salesDetail": des,
-			"salesPwd": pass,
-			"code": code
-		};
-		
-		if(pass != repass){
-			alert("两次密码不一致");
-		}
-		else if(username == "" || name == "" || address == "" || phone == "" ||
-			pass == "" || repass == "" || des == "" || code == ""){
-			alert("请填写完整的资料");
+		var pattern = /^\d{11}$/;
+		if(!pattern.test(phone)){
+			alert("请输入正确的手机号码！");
 		}
 		else{
 			$.ajax({
@@ -119,10 +42,47 @@ $(document).ready(()=>{
 				url:"",
 				async:true,
 				dataType:"json",
-				data: merchantReg,
+				data: {
+					"phone": phone
+				},
+				success:function(result){
+					alert("已发送验证码，请注意查收！");
+				},
+				error:function(result){
+					alert("验证码发送失败！");
+				}
+			});
+		}
+	})
+	
+	/*
+	 * 会员注册
+	 */
+	$("#member_reg").click(()=>{
+		var phone = $("#member_phone").val();
+		var code = $("#member_code").val();
+		var pwd = $("#member_pwd").val();
+		var repwd = $("#member_repwd").val();
+		
+		var User = {
+			"loginName": phone,
+			"code": code,
+			"password": pwd
+		};
+		
+		if(pwd != repwd){
+			alert("两次密码不一致!");
+		}
+		else{
+			$.ajax({
+				type:"post",
+				url:"/userRegister",
+				async:true,
+				dataType:"json",
+				data: User,
 				success:function(result){
 					alert("注册成功！");
-					history.go(-1);
+					window.location.href = "login.html?type=1";
 				},
 				error:function(result){
 					alert("注册失败！");
@@ -132,38 +92,48 @@ $(document).ready(()=>{
 	})
 	
 	/*
-	 * 忘记密码
+	 * 商家注册
 	 */
-	$("#change_pwd").click(()=>{
-		var phone = $("#pwd_phone").val();
-		var code = $("#pwd_code").val();
-		var pass = $("#pwd_pass").val();
-		var repass = $("#pwd_repass").val();
+	$("#merchant_reg").click(()=>{
+		var phone = $("#merchant_phone").val();
+		var code = $("#merchant_code").val();
+		var pwd = $("#merchant_pwd").val();
+		var repwd = $("#merchant_repwd").val();
+		var name = $("#merchant_name").val();
+		var address = $("#merchant_address").val();
 		
-		var changPwd = {
-			"username": phone,
-			"code": code,
-			"salesPwd": pass
+		var Sales = {
+			"salesName": name,
+			"selesPhone": phone,
+			"salesAddress": address,
+			"salesPwd": pwd,
+			"code": code
 		};
 		
-		if(pass != repass){
-			alert("两次密码不一致！");
+		if(pwd != repwd){
+			alert("两次密码不一致");
+		}
+		else if(name == "" || address == "" || phone == "" ||
+			pwd == "" || repwd == "" || code == ""){
+			alert("请填写完整的资料");
 		}
 		else{
-			$.ajax({
-				type:"post",
-				url:"",
-				async:true,
-					dataType:"json",
-				data: memberReg,
-				success:function(result){
-					alert("修改成功！");
-					history.go(-1);
-				},
-				error:function(result){
-					alert("修改失败！");
-				}
-			});
+			alert("注册成功！");
+			window.location.href = "login.html?type=2";
+//			$.ajax({
+//				type:"post",
+//				url:"/salesRegister",
+//				async:true,
+//				dataType:"json",
+//				data: Sales,
+//				success:function(result){
+//					alert("注册成功！");
+//					window.location.href = "login.html?type=2";
+//				},
+//				error:function(result){
+//					alert("注册失败！");
+//				}
+//			});
 		}
 	})
 	
