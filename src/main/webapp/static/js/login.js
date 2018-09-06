277fee4dc35e6d5e5a244fae6ff2cf9e7c273058
GRATTFISH <38063466+GRATTFISH@users.noreply.github.com>
2018-09-06 15:11:31 +0800

    改版登录

--- src/main/webapp/static/js/login.js
+++ src/main/webapp/static/js/login.js
@@ -7,58 +7,26 @@ function getUrlParam(name) {
     if (r != null) return decodeURI(r[2]); return null; 
 }
 
-/*
- * 获取活动内容
- */
-new Vue({
-    el: '#left',
-    data() {
-        return {
-        	sites: ""
-        }
-    },
-    created: function(){
-    	var self = this;
-   		$.ajax({
-			type:"get",
-			url:"../static/json/activity.json",
-			async:true,
-			dataType:"json",
-			success:function(inf){
-				self.sites = inf;
-			},
-			error:function(inf){
-				alert("获取数据失败！");
-			},
-		});
- 	}
-});
-
-/*
- * 登录切换
- */
-var right = {
-    data() {
-      	return {
-        	activeName: 'first'
-      	};
-    },
-    methods: {
-      	handleClick(tab, event) {
-        	console.log(tab, event);
-      	}
-    }
-};
-var Ctor = Vue.extend(right);
-new Ctor().$mount('#right');
-
 $(document).ready(()=>{
+	
+	$(()=>{
+		var type = getUrlParam('type');
+		if(type == 1){
+			document.getElementById("title").innerHTML = "会员登录";
+		}
+		else if(type == 2){
+			document.getElementById("title").innerHTML = "商家登录";
+		}
+		else{
+			document.getElementById("title").innerHTML = "登录商城";
+		}
+	})
 	
 	/*
 	 * 密码登录
 	 */
-	$("#login_pass").click(()=>{
-		var phone = $("#phone_pass").val();
+	$("#login").click(()=>{
+		var phone = $("#user").val();
 		var pass = $("#password").val();
 		var type = getUrlParam('type');
 		var passLogin = {
@@ -67,63 +35,25 @@ $(document).ready(()=>{
 			"type": type
 		}
 		
-		$.ajax({
-			type:"post",
-			url:"",
-			async:true,
-			dataType:"json",
-			data: passLogin,
-			success:function(result){
-				alert("登录成功！");
-				window.location.href = "#";
-			},
-			error:function(result){
-				alert("登录出错！");
-			}
-		});
-	});
-	
-	/*
-	 * 获取验证码
-	 */
-	$("#code_btn").click(()=>{
-		var phone = $("#phone_code").val();
 		var pattern = /^\d{11}$/;
 		if(!pattern.test(phone)){
 			alert("请输入正确的手机号码！");
 		}
 		else{
-			alert(phone);
+			$.ajax({
+				type:"post",
+				url:"/userLogin",
+				async:true,
+				dataType:"json",
+				data: passLogin,
+				success:function(result){
+					alert("登录成功！");
+					window.location.href = "home.html";
+				},
+				error:function(result){
+					alert("登录出错！");
+				}
+			});
 		}
-	})
-	
-	/*
-	 * 验证登录
-	 */
-	$("#login_code").click(()=>{
-		var phone = $("#phone_code").val();
-		var code = $("#code").val();
-		var type = getUrlParam('type');
-		var codeLogin = {
-			"loginName": phone,
-			"loginPwd": pass,
-			"type": type
-		}
-		
-		$.ajax({
-			type:"post",
-			url:"",
-			async:true,
-			dataType:"json",
-			data: codeLogin,
-			success:function(result){
-				alert("登录成功！");
-				window.location.href = "#";
-			},
-			error:function(result){
-				alert("登录出错！");
-			}
-		});
 	});
-	
 })
