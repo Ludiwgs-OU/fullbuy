3d69ccddc11c7a7e934747db6f416c88db9a852b
GRATTFISH <38063466+GRATTFISH@users.noreply.github.com>
2018-09-19 09:25:10 +0800

    收藏和评价

---
+++ src/main/webapp/static/js/coll.js
@@ -0,0 +1,67 @@
+new Vue({
+	el: '#headerNav',
+	data:function(){
+   		return {
+   			site: ""
+   	 	}
+ 	},
+	created: function(){
+    	var self = this;
+   		$.ajax({
+			type:"get",
+			url:"../static/json/login.json",
+			async:true,
+			dataType:"json",
+			success:function(inf){
+				self.site = inf;
+			},
+			error:function(inf){
+				alert("获取数据失败！");
+			},
+		});
+   	}
+})
+
+new Vue({
+	el: '#coll-list',
+	data:function(){
+   		return {
+   			sites: ""
+   	 	}
+ 	},
+	created: function(){
+    	var self = this;
+   		$.ajax({
+			type:"get",
+			url:"../static/json/coll.json",
+			async:true,
+			dataType:"json",
+			success:function(inf){
+				self.sites = inf;
+			},
+			error:function(inf){
+				alert("获取数据失败！");
+			},
+		});
+   	},
+   	methods: {
+   		calColl: function(id){
+   			$.ajax({
+				type:"post",
+				url:"#",
+				async:true,
+				dataType:"json",
+				data: {
+					"favorId": id
+				},
+				success:function(inf){
+					alert("取消收藏成功！");
+					window.reload();
+				},
+				error:function(inf){
+					alert("取消收藏失败！");
+				},
+			});
+   		}
+   	}
+})
